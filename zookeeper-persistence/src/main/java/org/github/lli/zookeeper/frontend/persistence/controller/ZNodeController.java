5230e0336e66b12175e3fdd519143f62205eb0fb
startupleo2013 <startupleo2013@gmail.com>
2013-11-17 20:57:26 -0500

    added zookeeper-persistence project

---
+++ zookeeper-persistence/src/main/java/org/github/lli/zookeeper/frontend/persistence/controller/ZNodeController.java
@@ -0,0 +1,32 @@
+package org.github.lli.zookeeper.frontend.persistence.controller;
+
+import java.util.List;
+
+import org.github.lli.zookeeper.frontend.persistence.model.RESTFulServiceUtils;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * 
+ * @author startupleo2013
+ * 
+ */
+@Controller
+public class ZNodeController {
+
+	@RequestMapping(value = "/query/all", method = RequestMethod.GET)
+	@ResponseBody
+	public void getAll() {
+		try {
+			List<String> nodes = RESTFulServiceUtils.getCuratorFramework()
+					.getChildren().forPath("/");
+			for(String s: nodes)
+				System.out.println(s);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
---
+++ zookeeper-persistence/src/main/java/org/github/lli/zookeeper/frontend/persistence/model/RESTFulServiceUtils.java
@@ -0,0 +1,47 @@
+package org.github.lli.zookeeper.frontend.persistence.model;
+
+import java.io.IOException;
+import java.util.Properties;
+
+import org.apache.curator.RetryPolicy;
+import org.apache.curator.framework.CuratorFramework;
+import org.apache.curator.framework.CuratorFrameworkFactory;
+import org.apache.curator.retry.ExponentialBackoffRetry;
+import org.apache.log4j.Logger;
+
+/***
+ * 
+ * @author startupleo2013
+ * 
+ */
+public class RESTFulServiceUtils {
+	private static Logger logger = Logger.getLogger(RESTFulServiceUtils.class);
+	private static final String CURATOR_PROPERTIES_FILE = "/curator.properties";
+	private static final String MAX_RETRIES = "max.retries";
+	private static final String CONNECTION_STRING = "connection.string";
+	private static CuratorFramework client;
+
+	static {
+		initClient();
+	}
+
+	public synchronized static CuratorFramework getCuratorFramework() {
+		if (client == null)
+			initClient();
+		return client;
+	}
+
+	private static void initClient() {
+		try {
+			Properties props = new Properties();
+			props.load(RESTFulServiceUtils.class
+					.getResourceAsStream(CURATOR_PROPERTIES_FILE));
+			int maxRetries = Integer.parseInt(props.getProperty(MAX_RETRIES));
+			RetryPolicy policy = new ExponentialBackoffRetry(1000, maxRetries);
+			client = CuratorFrameworkFactory.newClient(
+					props.getProperty(CONNECTION_STRING), policy);
+		} catch (IOException e) {
+			logger.fatal("Failed to load " + CURATOR_PROPERTIES_FILE);
+		}
+	}
+}
